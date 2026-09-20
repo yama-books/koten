112b0fbf1097385b491867dc2744b14598e38aed
yama-books <y20140148@gmail.com>
2026-09-20 12:36:39 +0900

    production: simplify checkpoint public UI (checkpoint/core1.js)

--- checkpoint/core1.js
+++ checkpoint/core1.js
@@ -63,7 +63,6 @@ let currentDrawerHit = null;
 let currentDrawerHits = [];
 let currentPointIndex = 0;
 let currentSurfaceText = "";
-let currentCompletedPointKeys = new Set();
 let drawerNeedsRender = false;
 
 function hitKey(h){ return `${h.start}:${h.end}:${h.pattern}:${h.type}`; }
